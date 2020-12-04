94c61d99d3b2144c5870bc2d8600a0faab2442bc
Bluebird <stephany1445@hotmail.com>
2020-12-03 18:59:26 -0800

    Added validation to email to ensure its valid

--- models/User.js
+++ models/User.js
@@ -7,9 +7,11 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      match: [`/.+\@.+\..+/`, "Please enter a valid e-mail address"]
     },
     password: {
       type: String,
+      validate:[({length})=> length>=8, "Password should be at least 8 characters long"]
     },
     date: {
       type: Date,
