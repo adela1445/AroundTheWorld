aff2e74174a89e6b37bec4b5597912948b8f4ef9
Bluebird <stephany1445@hotmail.com>
2020-12-05 10:09:16 -0800

    App is working. Fixed authentication

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,14 +3,14 @@ import logo from './logo.svg'
 import './Home.css'
 
 function Home() {
-  
   return (
     <div>
-      <img src={logo} />
+      {/* <img src={logo} /> */}
       <h1>Hello</h1>
       <h2>
         {' '}
-        You're now inside a protected route, you can only access this if you are logged in.
+        You are now inside a protected route, you can only access this if you
+        are logged in.
       </h2>
     </div>
   )
